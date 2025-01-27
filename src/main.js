f4f809a3dc9436a1d6b0fe5fbfe5784cd4fc789c
Nico Carra <nicolocarraro2@gmail.com>
2025-01-27 20:12:28 +0100

    added api

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+const apiKey = process.env.API_KEY
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=vicenza" //url from openweathermap.org 2.5
+
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+  var data = await response.json()
+
+  console.log(data)
+
+  document.querySelector(".city").innerHTML = data.name
+  document.querySelector(".temp").innerHTML = data.main.temp + '°c'
+  document.querySelector(".humidity").innerHTML = data.main.humidity + '%'
+  document.querySelector(".wind").innerHTML = data.wind.speed + ' km/h'
+
+  
+}
+
+searchBtn.addEventListener('click', () => {
+  checkWeather(searchBox.value)
+})
+
+
+
 
