b441af9686744ee92736a3b40a6a2480eacc581e
Nico Carra <nicolocarraro2@gmail.com>
2025-01-28 13:35:59 +0100

    fixed search

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
 const apiKey = import.meta.env.VITE_API_KEY //added in netlify
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=vicenza" //url from openweathermap.org 2.5
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=" //url from openweathermap.org 2.5
 
 const searchBox = document.querySelector('.search input')
 const searchBtn = document.querySelector('.search button')
